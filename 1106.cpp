7df87d46cd7ed99d4beb4be0d54e20f63837292d
Taras Koroliuk <taras.koroliuk@globallogic.com>
2014-05-27 20:14:08 +0300

    1106

--- 1106.cpp
+++ 1106.cpp
@@ -1,19 +1,80 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <map>
+#include <set>
+#include <vector>
 #include <cassert>
 #include <cstdlib>
 
 using namespace std;
 
-typedef pair<int, int> link_t;
-multimap<link_t, int> suite;
+class X {
+	static int cnt;
+	int id;
+	int team;
+	set<X*> friends;
+public:
+	X(): id(cnt++), team(0) {}
+	friend
+	ostream& operator<<(ostream& os, const X& x) {
+		os << x.id;
+	}
+	operator bool() const { return id != cnt - 1; }
+	void switch_team() { team ^= 1; }
+	bool has_opposite() const {
+		for (auto& fr : friends)
+			if (team != (*fr).team)
+				return true;
+		return false;
+	}
+	void add_friend(X* px) {
+		friends.insert(px);
+	}
+	void print() const {
+		cout << *this << '(' << this->team << ')' << "-> ";
+		for (auto& fr : friends)
+			cout << *fr << ' ';
+		cout << endl;
+	}
+};
 
-const int team_A = 0;
-const int team_B = 1;
+int X::cnt = 1; // first element
 
-void process_input()
+class Suite {
+	X *suite;
+public:
+	Suite(int sz) {
+		suite = new X[sz + 1];
+	}
+	~Suite() {
+		delete []suite;
+	}
+	X& operator[](int n) {
+		return suite[n - 1];
+	}
+	int colision_rate() const;
+	void print() const;
+};
+
+
+int Suite::colision_rate() const
+{
+	int rate = 0;
+	for (X *px = suite; *px; px++)
+		if (!px->has_opposite()) rate++;
+
+	return rate;
+}
+
+void Suite::print() const
+{
+	for (X *px = suite; *px; px++)
+		px->print();
+	cout << "Rate: " << this->colision_rate() << endl;
+}
+
+//void process_input()
+int main()
 {
 	string line, word;
 
@@ -22,73 +83,41 @@ void process_input()
 	int n = atoi(line.c_str());
 	assert(n >= 1 && n <= 100);
 
-	for (int i = 1; i <= n && getline(cin, line); i++) {
-		istringstream is(line);
-		int friend1 = i;
-		while (is >> word) {
-			if (word == "0") break;
+	Suite suite(n);
 
-			int friend2 = atoi(word.c_str());
-			suite.insert({{friend1, friend2}, team_A});
+	for (int i = 1; i <= n && getline(cin, line); ++i) {
+		istringstream is(line);
+		while (is >> word && word != "0") {
+			int num = atoi(word.c_str());
+			suite[i].add_friend(&suite[num]);
 		}
 	}
+
+	suite.print();
+	for (int i = 1; i <= n; i++) {
+		if (!suite[i].has_opposite())
+			suite[i].switch_team();
+	}
+	cout << endl;
+	suite.print();
 }
 
+#if 0
 ostream& operator<<(ostream& os, const pair<link_t, int>& elem)
 {
 		os << elem.first.first << "->" << elem.first.second << ": "
 			<< elem.second;
 		return os;
 }
-
 template<class T>
 void print(T& container)
 {
 	for (auto& elem : container)
 		cout << elem << endl;
 }
-
-void toggle_switch(pair<const link_t, int>& link)
-{
-	link.second = (link.second == team_A) ? team_B : team_A;
-}
-
 int main()
 {
 	process_input();
-
-	bool balanced;
-	int team_ba, key_ba;
-	multimap<link_t, int>::iterator it_ba;
-
-	int cnt {0};
-	do {
-		balanced = true;
-		cout << "Num: " << ++cnt << endl;
-		cout << "A -> B: "<< endl;
-		print(suite);
-		for (auto pos_ab = suite.begin(); pos_ab != suite.end(); ++pos_ab)
-			for (auto pos_ba = suite.begin(); pos_ba != suite.end(); ++pos_ba) {
-				if (pos_ab->first.first == pos_ba->first.second &&
-						pos_ab->first.second == pos_ba->first.first) {
-					if (pos_ab->second == pos_ba->second) {
-						toggle_switch(*pos_ba);
-						balanced = false;
-						key_ba = pos_ba->first.first;
-						team_ba = pos_ba->second;
-						it_ba = pos_ba;
-						goto switch_others;
-					}
-				}
-			}
-switch_others:
-		cout << "A -> B: "<< endl;
-		print(suite);
-		if (!balanced)
-			for (auto pos = it_ba; pos != suite.end(); ++pos)
-				if (pos->first.first == key_ba && pos->second != team_ba) {
-					toggle_switch(*pos);
-			}
-	} while (!balanced);
-
 }
+#endif
+
